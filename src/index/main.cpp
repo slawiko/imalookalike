c7373ed0552954d3b785f41dff4365cf59965bef
batist73 <agrigoryev95@gmail.com>
2019-04-01 20:57:49 +0300

    add web-server with API

--- src/index/main.cpp
+++ src/index/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 
 #include "index.h"
+#include "httplib.h"
 
 std::vector<double> parseDescriptor(std::istream &in) {
 	std::vector<double> descriptor;
@@ -46,6 +47,42 @@ Index loadIndex(std::string fileName) {
 
 int main() {
 	Index index = loadIndex("index.data");
+	httplib::Server app;
+
+	app.Get("/health", [](const httplib::Request&, httplib::Response &res) {
+		res.set_content("I'm OK", "text/plain");
+	});
+
+	app.Post("/neighbour", [&index](const httplib::Request &req, httplib::Response &res) {
+		std::istringstream bodyStream(req.body);
+		std::vector<double> descriptor = parseDescriptor(bodyStream);
+
+		// TODO: check descriptor size
+
+		SearchResult searchResult = index.search(descriptor, 1)[0];
+
+		// TODO: handle empty result
+
+		std::ifstream file(searchResult.name, std::ios::binary | std::ios::ate);
+
+		int size = file.tellg();
+		char *image = new char[size];
+
+		file.seekg(0, std::ios::beg);
+		file.read(image, size);
+
+		res.set_content(image, size, "image/jpeg");
+		res.set_header("Name", searchResult.name.c_str());
+
+		// TODO: handle image extension
+
+		file.close();
+		delete[] image;
+	});
+
+	int port = 8000;
+	std::cout << "Server is listening on port " << port << std::endl;
+	app.listen("localhost", port);
 
 	return 0;
 }
