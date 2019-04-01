1bbad97c3e45616b04c26eaefb61864bea91808d
batist73 <agrigoryev95@gmail.com>
2019-04-01 18:34:01 +0300

    add index loading

--- src/index/main.cpp
+++ src/index/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "index.h"
 
+std::vector<double> parseDescriptor(std::istream &in) {
+	std::vector<double> descriptor;
+	std::string item;
+
+	while (getline(in, item, ',')) {
+		descriptor.push_back(stod(item));
+	}
+
+	return descriptor;
+}
+
+Index loadIndex(std::string fileName) {
+	// TODO: check file existence
+
+	std::ifstream file(fileName);
+	std::string line;
+
+	getline(file, line);
+
+	int descriptorSize = stoi(line);
+	Index index(descriptorSize);
+
+	while (getline(file, line)) {
+		// TODO: check reading
+
+		std::istringstream lineStream(line);
+
+		std::string name;
+		getline(lineStream, name, ',');
+
+		std::vector<double> descriptor = parseDescriptor(lineStream);
+
+		// TODO: check descriptor size
+
+		index.insert(std::move(name), std::move(descriptor));
+	}
+
+	return index;
+}
+
 int main() {
-	std::cout << "Hello, world!" << std::endl;
+	Index index = loadIndex("index.data");
+
+	return 0;
 }
