2cac5bd6531897d0a8c56ab02b8fc1addd5cc338
slawiko <shchaurouski.slava@gmail.com>
2019-04-13 17:14:11 +0300

    adds image sending/receiving logic

--- src/app/app.py
+++ src/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__, static_folder='../client/build', template_folder='../client/build')
 
@@ -9,5 +9,14 @@ def static_route(path):
 @app.route('/')
 def root():
   return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+  print(request)
+  data = request.data
+  with open('./camcrolinfasta.jpeg', 'wb') as f:
+    f.write(data)
+  
+  return 'OK', 200
 
 app.run(host='0.0.0.0', port=8000, debug=None)
--- src/client/src/components/main/Main.tsx
+++ src/client/src/components/main/Main.tsx
@@ -2,12 +2,41 @@ import * as React from 'react';
 
 import './Main.css';
 
-export class Main extends React.Component {
+export class Main extends React.Component<{}> {
+  constructor(props: {}) {
+    super(props);
+    this.handleFileUpload = this.handleFileUpload.bind(this);
+  }
+
   public render() {
     return (
       <div className="main">
-        <p>Hi team 2</p>
+        <input type="file" onChange={this.handleFileUpload} />
       </div>
     );
+  }
+
+  public handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const formData = new FormData();
+    if (!event.target.files) {
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsArrayBuffer(event.target.files[0]);
+
+    reader.addEventListener('loadend', e => {
+      fetch('/upload', {
+        method: 'POST',
+        body: reader.result,
+        headers: {
+          'Content-type': 'image/jpeg'
+        }
+      })
+      .then(res => {
+        console.log(res);
+      });
+    });
   }
 }
