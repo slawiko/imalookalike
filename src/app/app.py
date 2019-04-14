5cbda2436f6ebff12e1f2ab4f60d3c3f37c4af54
slawiko <shchaurouski.slava@gmail.com>
2019-04-14 12:26:27 +0300

    changes file location again

--- src/app/app.py
+++ src/app/app.py
@@ -2,21 +2,25 @@ from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__, static_folder='../client/build', template_folder='../client/build')
 
+
 @app.route('/<path:path>')
 def static_route(path):
-  return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.static_folder, path)
+
 
 @app.route('/')
 def root():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
-  print(request)
-  data = request.data
-  with open('./camcrolinfasta.jpeg', 'wb') as f:
-    f.write(data)
-  
-  return 'OK', 200
+    print(request)
+    data = request.data
+    with open('./camcrolinfasta.jpeg', 'wb') as f:
+        f.write(data)
+
+    return 'OK', 200
+
 
 app.run(host='0.0.0.0', port=8000, debug=None)
