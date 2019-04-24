c4fa0ba181b9bbc2a487411298d19633420d9c11
DaryaRovdo <d.rovdo@gmail.com>
2019-04-24 16:08:46 +0300

    Move triplet loss

--- src/app/server/net/utils/train_model.py
+++ src/app/server/net/utils/train_model.py
@@ -2,7 +2,7 @@
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from src.net.utils.triplet_loss import batch_all_triplet_loss
+from src.app.server.net.utils.triplet_loss import batch_all_triplet_loss
 import sys
 import os
 import csv
