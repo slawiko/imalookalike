31e618cde63c34413d631f69af84fae2c69c842f
slawiko <shchaurouski.slava@gmail.com>
2019-04-28 16:50:30 +0300

    adds request to index

--- src/app/server/app.py
+++ src/app/server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, make_response
+import requests
 import imghdr
 
 from errors import InvalidImage
@@ -30,11 +31,15 @@ def upload():
         raise InvalidImage()
     try:
         result = process_image(request.data)
+        data = ','.join([str(e) for e in result[0]])
+        image = requests.post('http://localhost:8001/neighbour', data=data, headers={'content-type': 'text/plain'})
+        response = make_response(image.content)
+        response.headers.set('Content-Type', 'image/jpeg')
     except Exception as error:
         print(error)
         return 'Internal server error', 500
 
-    return str(result), 200
+    return response
 
 
 app.run(host='0.0.0.0', port=8000, debug=None)
